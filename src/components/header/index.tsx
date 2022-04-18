1ed70ddd144323caabeb945b6ce009bd92e20204
shurintou <shurintou@gmail.com>
2022-04-18 23:01:35 +0900

    refactor header/index.tsx, add Home, Blogs, About tabs. create header/index.module.css

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,69 @@
 
-import { Component } from 'react'
-import { Layout, Menu } from 'antd';
+import React from 'react'
+import { Layout, Row, Col } from 'antd';
+import { HomeOutlined, ReadOutlined, UserOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 const { Header } = Layout;
 
-export default class BlogHeader extends Component {
+interface antdColPropObj {
+    xs: antdColSubPropObj,
+    sm: antdColSubPropObj,
+    md: antdColSubPropObj,
+    lg: antdColSubPropObj,
+    xl: antdColSubPropObj,
+}
+
+interface antdColSubPropObj {
+    span?: number,
+    offset?: number,
+}
+
+export default class BlogHeader extends React.Component<{}, { [key: string]: any }> {
+
     render() {
+        const spanPropObj: antdColPropObj = {
+            'xs': { span: 6 },
+            'sm': { span: 5 },
+            'md': { span: 4 },
+            'lg': { span: 3 },
+            'xl': { span: 2 },
+        }
+        const offsetPropObj: antdColPropObj = {
+            'xs': { offset: 0 },
+            'sm': { offset: 1 },
+            'md': { offset: 2 },
+            'lg': { offset: 3 },
+            'xl': { offset: 4 },
+        }
         return (
-            <Header>
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                </Menu>
+            <Header style={{ 'padding': '0 1em' }}>
+                <Row>
+                    <Col
+                        {   /* return a new object that has xs,sm,md,lg,xl properties, 
+                                in which containing both span and offset properties copied from spanPropObj and offsetPropObj.
+                            */
+                        ...Object.assign({},
+                            Object.fromEntries(Object.keys(spanPropObj).map(
+                                key =>
+                                    [
+                                        key,
+                                        Object.assign({},
+                                            spanPropObj[key as keyof antdColPropObj],
+                                            offsetPropObj[key as keyof antdColPropObj])
+                                    ]
+                            ))
+                        )
+                        }
+                    >
+                        <Button type="primary" icon={<HomeOutlined />}>Home</Button>
+                    </Col>
+                    <Col {...spanPropObj}>
+                        <Button type="primary" icon={<ReadOutlined />}>Blogs</Button>
+                    </Col>
+                    <Col {...spanPropObj}>
+                        <Button type="primary" icon={<UserOutlined />}>About</Button>
+                    </Col>
+                </Row>
             </Header>
         )
     }
