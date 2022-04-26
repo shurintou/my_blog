d062d76d203d22cd98e2ca0f230cf5111be64dbd
shurintou <shurintou@gmail.com>
2022-04-27 01:52:42 +0900

    feature(prerender): 1.Add prerender-spa-plugin dependency to prerender spa into html.(Because of https://github.com/chrisvfritz/prerender-spa-plugin/issues/414#issuecomment-778660698, use forked project instead.) 2.Add react-app-rewired dependency to allow modification of the webpack configuration. 3.Add config-overrides.js to configure react-app-rewired. 4.Add concurrently dependency to run npm script "craco" and "react-app-rewired" concurrently.

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('@dreysolano/prerender-spa-plugin')
+const overrideFunc = function override(config, env) {
+    if (env === 'production') {
+        config.plugins = config.plugins.concat([
+            new PrerenderSPAPlugin({
+                routes: ['/', '/about', '/blogs'],
+                staticDir: path.join(__dirname, 'build'),
+            }),
+        ])
+    }
+    return config
+}
+
+module.exports = overrideFunc
