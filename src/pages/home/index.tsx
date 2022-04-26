acb11016acfac032d056acac2550ae18d5946755
shurintou <shurintou@gmail.com>
2022-04-26 23:29:50 +0900

    style(semi colon): revoke changes that remove ";" from "&nbsp;".

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -27,17 +27,17 @@ export default class Home extends React.Component<{}, { [key: string]: any }> {
                             <Title style={{ marginTop: '1em' }}>Hello.</Title>
                             <Title level={3}>My name is Shurintou.</Title>
                             <Paragraph>
-                                <p className={homeStyle.homeText}>I'm a&nbsp
+                                <p className={homeStyle.homeText}>I'm a&nbsp;
                                     <Text underline><Link href="https://en.wikipedia.org/wiki/Web_developer" target="_blank">Web developer</Link></Text>
                                     , now working in Tokyo.</p>
                                 <p className={homeStyle.homeText}>I love coding and want to become a full stack engineer.</p>
                                 <p className={homeStyle.homeText}>I mainly write program in Javascript (
-                                    <Text underline><Link href='https://vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp
+                                    <Text underline><Link href='https://vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp;
                                     <Text underline><Link href='https://reactjs.org/' target="_blank">React</Link></Text>
                                     ) and Java (
                                     <Text underline><Link href='https://spring.io/' target="_blank">Spring</Link></Text>
                                     ).</p>
-                                <p className={homeStyle.homeText}>If you happen to be a programmer, you can follow me on my&nbsp
+                                <p className={homeStyle.homeText}>If you happen to be a programmer, you can follow me on my&nbsp;
                                     <Text underline><Link href='https://github.com/shurintou' target="_blank">Github</Link></Text>
                                     .</p>
                                 <p className={homeStyle.homeText}>In my spare time, I enjoy karaoke, cooking food and playing badminton.</p>
