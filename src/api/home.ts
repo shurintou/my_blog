804c79b514a405e31af95d29734f2efed7ec59a7
shurintou <shurintou@gmail.com>
2022-04-29 01:52:15 +0900

    chore: remove home.ts which is not used.

--- src/api/home.ts
+++
@@ -1,9 +0,0 @@
-import request from '../utils/request'
-
-export function getProfileInfo(params: Object) {
-    return request({
-        url: 'https://api.github.com/users/shurintou',
-        method: 'get',
-        params: params
-    })
-}
