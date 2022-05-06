4a16096276e073a01744aef27ff4af8a072d4a45
shurintou <shurintou@gmail.com>
2022-05-06 17:58:32 +0900

    chore(blog/index.tsx): modify the structure of blog page.

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
+import { Layout } from 'antd'
+import BlogsList from '../../components/blog'
 
 export default class Blogs extends Component {
     render() {
         return (
-            <div>index</div>
+            <Layout>
+                <BlogsList></BlogsList>
+            </Layout>
         )
     }
 }
