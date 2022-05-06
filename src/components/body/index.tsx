bbecf4cd4dfaba7cacb8dde52c853af78ca46635
shurintou <shurintou@gmail.com>
2022-05-06 17:57:47 +0900

    chore(router): add blog/:blogId router.

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from "react-router-dom"
 import Home from '../../pages/home'
 import Blogs from '../../pages/blogs'
 import About from '../../pages/about'
+import Blog from '../../pages/blog'
 
 export default class BlogBody extends Component {
     render() {
@@ -11,6 +12,7 @@ export default class BlogBody extends Component {
             <Layout style={{ margin: '2em 1em' }}>
                 <Routes>
                     <Route path='/home' element={<Home />}></Route>
+                    <Route path='/blog/:blogId' element={<Blog />}></Route>
                     <Route path='/blogs' element={<Blogs />}></Route>
                     <Route path='/about' element={<About />}></Route>
                     <Route path='/*' element={<Navigate to='/home' />}></Route>
