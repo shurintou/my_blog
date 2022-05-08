f70ce4f177c67b82e17917d844f26a8364d223b8
shurintou <shurintou@gmail.com>
2022-05-09 02:26:50 +0900

    fix(gitalk Bad credentials error): Add eventlistener to handler the 'Bad credentials' error that be thrown from gitalk but not be caught. ref https://javascript.info/promise-error-handling

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -1,12 +1,33 @@
-import { Component } from 'react'
-import { Layout } from 'antd'
+import React from 'react'
+import { Layout, message } from 'antd'
 import { Routes, Route, Navigate } from "react-router-dom"
 import Home from '../../pages/home'
 import Blogs from '../../pages/blogs'
 import About from '../../pages/about'
 import Blog from '../../pages/blog'
 
-export default class BlogBody extends Component {
+export default class BlogBody extends React.Component<{}, { [key: string]: any }> {
+    constructor(props: Object) {
+        super(props)
+        this.state = {
+            /* handler the 'Bad credentials' error that be thrown from gitalk but not be caught */
+            unhandledrejectionFunc: function (event: PromiseRejectionEvent | null) {
+                const res = event?.reason.response
+                if (res?.status === 401) {
+                    message.warning('Please login your github account first.')
+                }
+            }
+        }
+
+    }
+
+    componentDidMount() {
+        window.addEventListener('unhandledrejection', this.state.unhandledrejectionFunc)
+    }
+    componentWillUnmount() {
+        window.removeEventListener('unhandledrejection', this.state.unhandledrejectionFunc)
+    }
+
     render() {
         return (
             <Layout style={{ margin: '2em 1em' }}>
