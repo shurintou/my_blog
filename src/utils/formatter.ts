3e0ea9e4d564223672d6c76349119da9dd54af2a
shurintou <shurintou@gmail.com>
2022-05-09 23:11:04 +0900

    chore(formatter): 1.modify parseISODate func to return Date. 2.add parseISODateStr func that do exactly same as parseISODate. 3.add dependency of moment.

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,6 +1,15 @@
 import { parseISO } from 'date-fns'
+import moment from 'moment'
 
 export const parseISODate = function (date: string) {
+    return parseISO(date)
+}
+
+export const parseISODateStr = function (date: string) {
     const parsedDate: Date = parseISO(date)
     return parsedDate.toDateString() + ' ' + parsedDate.toLocaleTimeString()
+}
+
+export const getDateFromNow = function (date: Date) {
+    return moment(moment(date, "YYYYMMDD"), "YYYYMMDD").fromNow()
 }
