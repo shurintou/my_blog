6b27fcf7200b88434db63702934844766f3a1a1b
shurintou <shurintou@gmail.com>
2022-05-21 20:01:51 +0900

    fix(space): to add a space between 'bearer ' + getGitAccessToken().

--- src/api/blogs.ts
+++ src/api/blogs.ts
@@ -64,7 +64,7 @@ export function getReactionsByGraphQl(data: BlogGetLikeData) {
             `
         },
         headers: {
-            Authorization: 'bearer' + getGitAccessToken(),
+            Authorization: 'bearer ' + getGitAccessToken(),
         }
     })
 }
