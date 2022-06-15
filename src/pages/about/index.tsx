3446cb6733377c3cee820460fba3bf6c8a336698
shurintou <shurintou@gmail.com>
2022-06-16 01:21:08 +0900

    feature(image lazy load): to add lazy load feature in about page.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,23 +6,40 @@ import aboutStyle from './index.module.css'
 const { Text, Title, Link } = Typography
 
 export default class About extends React.Component<{}, { [key: string]: any }>  {
+    private imgDivRefs: Array<HTMLDivElement | null>
     constructor(props: Object) {
         super(props)
+        this.imgDivRefs = []
         this.state = {
-            windowInnerWdith: window.innerHeight,
+            loadImg: [false, false, false, false],
+            windowInnerWdith: window.innerHeight || document.documentElement.clientHeight,
             windowResizeFunc: () => {
-                this.setState({ windowInnerWdith: window.innerWidth })
+                this.setState({ windowInnerWdith: window.innerWidth || document.documentElement.clientHeight })
+            },
+            windowScrollFunc: () => {
+                let newLoadImg: Array<boolean> = this.state.loadImg
+                for (let i = 0; i < 4; i++) {
+                    const imgDiv = this.imgDivRefs[i]
+                    if (imgDiv && !newLoadImg[i]) {
+                        newLoadImg.splice(i, 1, this.state.windowInnerWdith - imgDiv.getBoundingClientRect().top > -100)
+                    }
+                }
+                this.setState({ loadImg: newLoadImg })
             }
         }
     }
 
+
     componentDidMount() {
+        this.state.windowScrollFunc()
         this.setState({ windowInnerWdith: window.innerWidth })
         window.addEventListener('resize', this.state.windowResizeFunc)
+        window.addEventListener('scroll', this.state.windowScrollFunc)
     }
 
     componentWillUnmount() {
-        window.removeEventListener('unhandledrejection', this.state.windowResizeFunc)
+        window.removeEventListener('resize', this.state.windowResizeFunc)
+        window.removeEventListener('scroll', this.state.windowScrollFunc)
     }
 
     render() {
@@ -36,8 +53,8 @@ export default class About extends React.Component<{}, { [key: string]: any }>  
             <Layout>
                 <Space direction="vertical" size={'large'}>
                     <Row>
-                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 12, push: 12 }} xl={{ span: 12, push: 12 }} style={{ height: imageContent1Height }}>
-                            <div className={aboutStyle['about-1']} style={{ width: '100%', height: '100%' }}></div>
+                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 12, push: 12 }} xl={{ span: 12, push: 12 }} style={{ height: imageContent1Height }} ref={el => this.imgDivRefs[0] = el}>
+                            {this.state.loadImg[0] && <div className={aboutStyle['about-1']} style={{ width: '100%', height: '100%' }}></div>}
                         </Col>
                         <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 12, pull: 12 }} xl={{ span: 12, pull: 12 }} style={{ height: textContent1Height }}>
 
@@ -56,8 +73,8 @@ export default class About extends React.Component<{}, { [key: string]: any }>  
                     </Row>
                     <Divider />
                     <Row>
-                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 10 }} xl={{ span: 10 }} style={{ height: imageContent2Height }}>
-                            <div className={aboutStyle['about-2']} style={{ width: '100%', height: '100%' }}></div>
+                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 10 }} xl={{ span: 10 }} style={{ height: imageContent2Height }} ref={el => this.imgDivRefs[1] = el}>
+                            {this.state.loadImg[1] && <div className={aboutStyle['about-2']} style={{ width: '100%', height: '100%' }}></div>}
                         </Col>
                         <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 14 }} xl={{ span: 14 }} style={{ height: textContent2Height }}>
                             <Layout style={{ padding: content2Padding }}>
@@ -75,8 +92,8 @@ export default class About extends React.Component<{}, { [key: string]: any }>  
                     </Row>
                     <Divider />
                     <Row>
-                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 10, push: 14 }} xl={{ span: 10, push: 14 }} style={{ height: imageContent2Height }}>
-                            <div className={aboutStyle['about-3']} style={{ width: '100%', height: '100%' }}></div>
+                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 10, push: 14 }} xl={{ span: 10, push: 14 }} style={{ height: imageContent2Height }} ref={el => this.imgDivRefs[2] = el}>
+                            {this.state.loadImg[2] && <div className={aboutStyle['about-3']} style={{ width: '100%', height: '100%' }} ></div>}
                         </Col>
                         <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 14, pull: 10 }} xl={{ span: 14, pull: 10 }} style={{ height: textContent2Height }}>
                             <Layout style={{ padding: content1Padding }}>
@@ -94,8 +111,8 @@ export default class About extends React.Component<{}, { [key: string]: any }>  
                     </Row>
                     <Divider />
                     <Row>
-                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 10 }} xl={{ span: 10 }} style={{ height: imageContent2Height }}>
-                            <div className={aboutStyle['about-4']} style={{ width: '100%', height: '100%' }}></div>
+                        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 10 }} xl={{ span: 10 }} style={{ height: imageContent2Height }} ref={el => this.imgDivRefs[3] = el}>
+                            {this.state.loadImg[3] && <div className={aboutStyle['about-4']} style={{ width: '100%', height: '100%' }} ></div>}
                         </Col>
                         <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 14 }} xl={{ span: 14 }} style={{ height: textContent2Height }}>
                             <Layout style={{ padding: content2Padding }}>
