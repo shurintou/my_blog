3dba59971d1450b45ec1f3102ac02ac86cea88b3
shurintou <shurintou@gmail.com>
2022-07-28 02:28:53 +0900

    feature(set html lang): 1. to add functions to get and set the html lang in userAgent.ts. 2. to set the html lang before the app render in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { Layout } from 'antd'
 import BlogHeader from './components/header'
 import BlogBody from './components/body'
 import BlogFooter from './components/footer'
+import { getLocalHtmlLang } from './utils/userAgent'
 import './App.less'
 
 function App() {
+    document.querySelector('html')?.setAttribute('lang', getLocalHtmlLang())
     return (
         <Layout>
             <BlogHeader />
--- src/utils/userAgent.ts
+++ src/utils/userAgent.ts
@@ -6,5 +6,30 @@ export function getBrowserLanguage() {
     const result = conf.languageProps.find(languageItem => browserLanguage.indexOf(languageItem.key) !== -1)
     if (result) return result
 
-    return conf.languageProps[0]
+    return conf.languageProps[0] //default language 'en'
+}
+
+const localStorageHtmlLangKey = 'html_lang'
+
+export function setLocalHtmlLang(lang: string) {
+    const lowerCaseLang = lang.toLowerCase()
+    document.querySelector('html')?.setAttribute('lang', lowerCaseLang)
+    if (languageExistInConfig(lowerCaseLang)) {
+        localStorage.setItem(localStorageHtmlLangKey, lowerCaseLang)
+    }
+    else {
+        localStorage.setItem(localStorageHtmlLangKey, getBrowserLanguage().key)
+    }
+}
+
+export function getLocalHtmlLang() {
+    const localHtmlLang = localStorage.getItem(localStorageHtmlLangKey)
+    if (localHtmlLang != null && languageExistInConfig(localHtmlLang)) {
+        return localHtmlLang
+    }
+    return getBrowserLanguage().key
+}
+
+function languageExistInConfig(lang: string | null) {
+    return conf.languageProps.some(language => language.key === lang)
 }
