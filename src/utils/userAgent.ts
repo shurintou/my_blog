9389f9572de85a9b671bfe7ef5422ee15cc21713
shurintou <shurintou@gmail.com>
2022-07-29 02:17:57 +0900

    chore(userAgent.ts): to not export getBrowserLanguage function in userAgent.ts anymore.

--- src/utils/userAgent.ts
+++ src/utils/userAgent.ts
@@ -1,6 +1,6 @@
 import conf from '../config/config'
 
-export function getBrowserLanguage() {
+function getBrowserLanguage() {
     const browserLanguage = navigator.language.toLowerCase()
 
     const result = conf.languageProps.find(languageItem => browserLanguage.indexOf(languageItem.key) !== -1)
