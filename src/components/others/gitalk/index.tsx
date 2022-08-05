4f812cb7d8c709b50f3ba50bead8a2a2b70f89ed
shurintou <shurintou@gmail.com>
2022-08-06 02:41:46 +0900

    feature(gitalk change language): to add a feature that gitalk dynamically change its language with the change of redux, by manually removing the old dom and calling the gitalk render api.

--- src/components/others/gitalk/index.tsx
+++ src/components/others/gitalk/index.tsx
@@ -3,23 +3,32 @@ import Gitalk from "gitalk"
 import 'gitalk/dist/gitalk.css'
 import { GitalkProps } from '../../../types/index'
 import config from '../../../config/config'
+import { useAppSelector } from '../../../redux/hooks'
 
 const GitalkCompo: React.FC<GitalkProps> = (props) => {
+    const selectedLanguage = useAppSelector((state) => state.language.value)
+
     const { blogId } = props
 
     useEffect(() => {
         function renderGitalk(blogId: number) {
-            const gitalkProps = { ...config.gitProps, number: blogId, perPage: config.blogProps.commentCountPerPage }
+            const gitalkProps = { ...config.gitProps, language: selectedLanguage, number: blogId, perPage: config.blogProps.commentCountPerPage }
             const gitalk = new Gitalk(gitalkProps)
+            const gitalkContainerEl = document.querySelector('#gitalk-container')
+            if (gitalkContainerEl) { // gitalk didn't offer any api to rerender the component so do it manually.
+                let childEls = gitalkContainerEl.childNodes
+                childEls.forEach(childEl => {
+                    gitalkContainerEl.removeChild(childEl)
+                })
+            }
             gitalk.render('gitalk-container')
         }
         renderGitalk(blogId)
 
-        /* eslint-disable-next-line */
-    }, [])
+    }, [blogId, selectedLanguage])
 
     return (
-        <div id="gitalk-container"></div>
+        <div id="gitalk-container" lang={selectedLanguage}></div>
     )
 }
 
