34447741767c8070b4812f41f2b5ec95b8b3fa90
shurintou <shurintou@gmail.com>
2022-08-09 02:43:08 +0900

    feature(home page change language): to add a feature that home page dynamically change its language with the change of redux.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,16 +2,19 @@ import React, { useEffect } from 'react'
 import { Layout, Row, Col, Typography } from 'antd'
 import BlogAvatar from '../../components/avatar'
 import homeStyle from './index.module.css'
+import { useAppSelector } from '../../redux/hooks'
+import { JA_LANGUAGE, ZH_LANGUAGE } from '../../config/constant'
 const { Title, Paragraph, Text, Link } = Typography
 
 const HomeCompo: React.FC<{}> = () => {
+    const selectedLanguage = useAppSelector((state) => state.language.value)
 
     useEffect(() => {
         window.scroll(0, 0)
     }, [])
 
     return (
-        <Layout style={{ padding: '0em 0.5em' }}>
+        <Layout style={{ padding: '0em 0.5em' }} lang={selectedLanguage}>
             <Row>
                 <Col
                     xs={{ offset: 0 }}
@@ -21,27 +24,64 @@ const HomeCompo: React.FC<{}> = () => {
                     xl={{ offset: 4 }}
                 >
                     <BlogAvatar></BlogAvatar>
-                    <Typography>
-                        <Title style={{ marginTop: '1em' }}>Hello.</Title>
-                        <Title level={3}>My name is Shurintou.</Title>
-                        <Paragraph>
-                            <p className={homeStyle.homeText}>I'm a&nbsp;
-                                <Text underline><Link href="https://en.wikipedia.org/wiki/Web_developer" target="_blank">Web developer</Link></Text>
-                                &nbsp;working in Tokyo.</p>
-                            <p className={homeStyle.homeText}>I love coding and want to become a full stack engineer.</p>
-                            <p className={homeStyle.homeText}>I mainly write program in Javascript (
-                                <Text underline><Link href='https://vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp;
-                                <Text underline><Link href='https://reactjs.org/' target="_blank">React</Link></Text>
-                                ) and Java (
-                                <Text underline><Link href='https://spring.io/' target="_blank">Spring</Link></Text>
-                                ).</p>
-                            <p className={homeStyle.homeText}>If you happen to be a programmer, you can follow me on my&nbsp;
-                                <Text underline><Link href='https://github.com/shurintou' target="_blank">Github</Link></Text>
-                                .</p>
-                            <p className={homeStyle.homeText}>In my spare time, I enjoy karaoke, cooking food and playing badminton.</p>
-                        </Paragraph>
-                    </Typography>
-
+                    {selectedLanguage === ZH_LANGUAGE.key ?
+                        <Typography>
+                            <Title style={{ marginTop: '0.5em' }}>Hi.</Title>
+                            <Title level={3} style={{ marginTop: '0.5em', marginBottom: '0.7em' }}>我是 Zhulintao.</Title>
+                            <Paragraph>
+                                <p className={homeStyle.homeText}>我在东京工作，是一名Web程序员。</p>
+                                <p className={homeStyle.homeText}>我热爱编程并想成为一名全栈工程师。</p>
+                                <p className={homeStyle.homeText}>我主要使用 Javascript (
+                                    <Text underline><Link href='https://cn.vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp;
+                                    <Text underline><Link href='https://zh-hans.reactjs.org/' target="_blank">React</Link></Text>
+                                    ) 和 Java (
+                                    <Text underline><Link href='https://spring.io/' target="_blank">Spring</Link></Text>
+                                    )语言。</p>
+                                <p className={homeStyle.homeText}>如果你碰巧也是一名程序员或开发者，欢迎关注我的&nbsp;
+                                    <Text underline><Link href='https://github.com/shurintou' target="_blank">Github</Link></Text>
+                                    。</p>
+                                <p className={homeStyle.homeText}>作为闲暇时的业余爱好，我喜欢唱歌、钻研各种美食和打羽毛球。</p>
+                            </Paragraph>
+                        </Typography>
+                        : selectedLanguage === JA_LANGUAGE.key ?
+                            <Typography>
+                                <Title style={{ marginTop: '0.5em' }}>こんにちは</Title>
+                                <Title level={3} style={{ marginTop: '0.5em', marginBottom: '0.7em' }}>しゅ りんとうと申します。</Title>
+                                <Paragraph>
+                                    <p className={homeStyle.homeText}>私は東京で働くWeb開発者です。</p>
+                                    <p className={homeStyle.homeText}>コーディングが好きで、フルスタックエンジニアになりたいです。</p>
+                                    <p className={homeStyle.homeText}>言語は、主に Javascript (
+                                        <Text underline><Link href='https://vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp;
+                                        <Text underline><Link href='https://ja.reactjs.org/' target="_blank">React</Link></Text>
+                                        ) と Java (
+                                        <Text underline><Link href='https://spring.io/' target="_blank">Spring</Link></Text>
+                                        )を使っています。</p>
+                                    <p className={homeStyle.homeText}>もしGithubアカウントをお持ちでしたら、
+                                        <Text underline><Link href='https://github.com/shurintou' target="_blank">フォロー</Link></Text>
+                                        をお願いいたします。</p>
+                                    <p className={homeStyle.homeText}>ちなみに、私の趣味は、カラオケ、料理を作ること、及びバドミントンです。</p>
+                                </Paragraph>
+                            </Typography>
+                            :
+                            <Typography>
+                                <Title style={{ marginTop: '0.5em' }}>Hello.</Title>
+                                <Title level={3} style={{ marginTop: '0.5em', marginBottom: '0.7em' }}>My name is Shurintou.</Title>
+                                <Paragraph>
+                                    <p className={homeStyle.homeText}>I'm a Web Developer working in Tokyo.</p>
+                                    <p className={homeStyle.homeText}>I love coding and want to become a Full Stack Engineer.</p>
+                                    <p className={homeStyle.homeText}>I mainly write program in Javascript (
+                                        <Text underline><Link href='https://vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp;
+                                        <Text underline><Link href='https://reactjs.org/' target="_blank">React</Link></Text>
+                                        ) and Java (
+                                        <Text underline><Link href='https://spring.io/' target="_blank">Spring</Link></Text>
+                                        ).</p>
+                                    <p className={homeStyle.homeText}>If you happen to be a programmer, you can follow me on my&nbsp;
+                                        <Text underline><Link href='https://github.com/shurintou' target="_blank">Github</Link></Text>
+                                        .</p>
+                                    <p className={homeStyle.homeText}>In my spare time, I enjoy karaoke, cooking food and playing badminton.</p>
+                                </Paragraph>
+                            </Typography>
+                    }
                 </Col>
             </Row>
         </Layout>
