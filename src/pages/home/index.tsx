31112193b2c769ff148da71edc42a20affd80237
shurintou <shurintou@gmail.com>
2022-08-09 00:39:16 +0900

    refactor(home/index.tsx): to refactor home component from class to hook.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,56 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Layout, Row, Col, Typography } from 'antd'
 import BlogAvatar from '../../components/avatar'
 import homeStyle from './index.module.css'
 const { Title, Paragraph, Text, Link } = Typography
 
+const HomeCompo: React.FC<{}> = () => {
 
-export default class Home extends React.Component<{}, { [key: string]: any }> {
-    constructor(props: Object) {
-        super(props)
-        this.state = {}
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         window.scroll(0, 0)
-    }
+    }, [])
 
-    render() {
-        return (
-            <Layout style={{ padding: '0em 0.5em' }}>
-                <Row>
-                    <Col
-                        xs={{ offset: 0 }}
-                        sm={{ offset: 2 }}
-                        md={{ offset: 2 }}
-                        lg={{ offset: 2 }}
-                        xl={{ offset: 4 }}
-                    >
-                        <BlogAvatar></BlogAvatar>
-                        <Typography>
-                            <Title style={{ marginTop: '1em' }}>Hello.</Title>
-                            <Title level={3}>My name is Shurintou.</Title>
-                            <Paragraph>
-                                <p className={homeStyle.homeText}>I'm a&nbsp;
-                                    <Text underline><Link href="https://en.wikipedia.org/wiki/Web_developer" target="_blank">Web developer</Link></Text>
-                                    &nbsp;working in Tokyo.</p>
-                                <p className={homeStyle.homeText}>I love coding and want to become a full stack engineer.</p>
-                                <p className={homeStyle.homeText}>I mainly write program in Javascript (
-                                    <Text underline><Link href='https://vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp;
-                                    <Text underline><Link href='https://reactjs.org/' target="_blank">React</Link></Text>
-                                    ) and Java (
-                                    <Text underline><Link href='https://spring.io/' target="_blank">Spring</Link></Text>
-                                    ).</p>
-                                <p className={homeStyle.homeText}>If you happen to be a programmer, you can follow me on my&nbsp;
-                                    <Text underline><Link href='https://github.com/shurintou' target="_blank">Github</Link></Text>
-                                    .</p>
-                                <p className={homeStyle.homeText}>In my spare time, I enjoy karaoke, cooking food and playing badminton.</p>
-                            </Paragraph>
-                        </Typography>
+    return (
+        <Layout style={{ padding: '0em 0.5em' }}>
+            <Row>
+                <Col
+                    xs={{ offset: 0 }}
+                    sm={{ offset: 2 }}
+                    md={{ offset: 2 }}
+                    lg={{ offset: 2 }}
+                    xl={{ offset: 4 }}
+                >
+                    <BlogAvatar></BlogAvatar>
+                    <Typography>
+                        <Title style={{ marginTop: '1em' }}>Hello.</Title>
+                        <Title level={3}>My name is Shurintou.</Title>
+                        <Paragraph>
+                            <p className={homeStyle.homeText}>I'm a&nbsp;
+                                <Text underline><Link href="https://en.wikipedia.org/wiki/Web_developer" target="_blank">Web developer</Link></Text>
+                                &nbsp;working in Tokyo.</p>
+                            <p className={homeStyle.homeText}>I love coding and want to become a full stack engineer.</p>
+                            <p className={homeStyle.homeText}>I mainly write program in Javascript (
+                                <Text underline><Link href='https://vuejs.org/' target="_blank">Vue</Link></Text>,&nbsp;
+                                <Text underline><Link href='https://reactjs.org/' target="_blank">React</Link></Text>
+                                ) and Java (
+                                <Text underline><Link href='https://spring.io/' target="_blank">Spring</Link></Text>
+                                ).</p>
+                            <p className={homeStyle.homeText}>If you happen to be a programmer, you can follow me on my&nbsp;
+                                <Text underline><Link href='https://github.com/shurintou' target="_blank">Github</Link></Text>
+                                .</p>
+                            <p className={homeStyle.homeText}>In my spare time, I enjoy karaoke, cooking food and playing badminton.</p>
+                        </Paragraph>
+                    </Typography>
 
-                    </Col>
-                </Row>
-            </Layout>
-        )
-    }
+                </Col>
+            </Row>
+        </Layout>
+    )
 }
+
+const HomeModule = () => <HomeCompo />
+export default HomeModule
