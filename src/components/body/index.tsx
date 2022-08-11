c6db248ea17e51b7999ff94e3f36532a73043017
shurintou <shurintou@gmail.com>
2022-08-11 17:14:48 +0900

    refactor(body): to refactor the blog component from class to hook. feature(body change language): to add a feature that body dynamically change its language with the change of redux.

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -1,30 +1,37 @@
-import React, { Suspense, lazy } from 'react'
+import React, { Suspense, lazy, useEffect } from 'react'
 import { Layout, message, Skeleton } from 'antd'
 import { Routes, Route, Navigate } from "react-router-dom"
 import { getGitUserInfo } from '../../api/user'
 import { setLocalUser } from '../../utils/authentication'
 import { GitUser } from '../../types/index'
 import { ROUTER_NAME } from '../../config/constant'
+import { useAppSelector, } from '../../redux/hooks'
+import { ZH_LANGUAGE, JA_LANGUAGE, EN_LANGUAGE, } from '../../config/constant'
 const Home = lazy(() => import(/* webpackChunkName: 'Home'*/ '../../pages' + ROUTER_NAME.home))
 const List = lazy(() => import(/* webpackChunkName: 'List'*/ '../../pages' + ROUTER_NAME.list))
 const About = lazy(() => import(/* webpackChunkName: 'About'*/ '../../pages' + ROUTER_NAME.about))
 const Post = lazy(() => import(/* webpackChunkName: 'Post'*/ '../../pages' + ROUTER_NAME.post))
 
-export default class BlogBody extends React.Component<{}, { [key: string]: any }> {
-    constructor(props: Object) {
-        super(props)
-        this.state = {
-            /* handler the 'Bad credentials' error that be thrown from gitalk but not be caught */
-            unhandledrejectionFunc: function (event: PromiseRejectionEvent | null) {
-                const res = event?.reason?.response
-                if (res?.status === 401) {
-                    message.warning('Please login your github account first.')
-                }
+const BlogBody: React.FC<{}> = () => {
+    const selectedLanguage = useAppSelector((state) => state.language.value)
+
+    const unhandledrejectionFunc = function (event: PromiseRejectionEvent | null) {
+        const res = event?.reason?.response
+        if (res?.status === 401) {
+            switch (selectedLanguage) {
+                case ZH_LANGUAGE.key:
+                    message.warning(ZH_LANGUAGE.loginMessage)
+                    break
+                case JA_LANGUAGE.key:
+                    message.warning(JA_LANGUAGE.loginMessage)
+                    break
+                default:
+                    message.warning(EN_LANGUAGE.loginMessage)
             }
         }
     }
 
-    componentDidMount() {
+    useEffect(() => {
         let failTolerantTime = 15
         let intervalId: NodeJS.Timeout
         if (process.env.NODE_ENV === 'production') {
@@ -42,25 +49,27 @@ export default class BlogBody extends React.Component<{}, { [key: string]: any }
                 }
             }, 1000)
         }
-        window.addEventListener('unhandledrejection', this.state.unhandledrejectionFunc)
-    }
-    componentWillUnmount() {
-        window.removeEventListener('unhandledrejection', this.state.unhandledrejectionFunc)
-    }
+        window.addEventListener('unhandledrejection', unhandledrejectionFunc)
 
-    render() {
-        return (
-            <Layout style={{ margin: '2em 0em' }}>
-                <Suspense fallback={<Skeleton active />}>
-                    <Routes>
-                        <Route path={ROUTER_NAME.home} element={<Home />}></Route>
-                        <Route path={ROUTER_NAME.post} element={<Post />}></Route>
-                        <Route path={ROUTER_NAME.list} element={<List />}></Route>
-                        <Route path={ROUTER_NAME.about} element={<About />}></Route>
-                        <Route path='/*' element={<Navigate to={ROUTER_NAME.home} />}></Route>
-                    </Routes>
-                </Suspense>
-            </Layout>
-        )
-    }
+        return () => {
+            window.removeEventListener('unhandledrejection', unhandledrejectionFunc)
+        }
+        /* eslint-disable-next-line */
+    }, [])
+
+    return (
+        <Layout style={{ margin: '2em 0em' }}>
+            <Suspense fallback={<Skeleton active />}>
+                <Routes>
+                    <Route path={ROUTER_NAME.home} element={<Home />}></Route>
+                    <Route path={ROUTER_NAME.post} element={<Post />}></Route>
+                    <Route path={ROUTER_NAME.list} element={<List />}></Route>
+                    <Route path={ROUTER_NAME.about} element={<About />}></Route>
+                    <Route path='/*' element={<Navigate to={ROUTER_NAME.home} />}></Route>
+                </Routes>
+            </Suspense>
+        </Layout>
+    )
 }
+
+export default BlogBody
